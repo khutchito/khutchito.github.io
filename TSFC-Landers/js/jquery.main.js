290a30d8c74f0dca85c9bf208e2ee604f7a23a4c
khutchito <37648782+khutchito@users.noreply.github.com>
2018-03-22 18:32:32 +0700

    Add files via upload

---
+++ TSFC-Landers/js/jquery.main.js
@@ -0,0 +1,441 @@
+// page init
+jQuery(window).load(function(){
+	initTooltip();
+	// initCustomHover();
+	jQuery('input, textarea').placeholder();
+	initValidation();
+});
+
+// hover tooltip init
+function initTooltip() {
+	jQuery('span[title]').hoverTooltip({
+		attribute: 'title',
+		positionTypeX: 'center',
+		positionTypeY: 'bottom'
+	});
+}
+
+// add classes on hover/touch
+function initCustomHover() {
+	jQuery('.video-holder .image-holder span').touchHover();
+}
+
+// form validation function
+function initValidation() {
+	var errorClass = 'error';
+	var successClass = 'success';
+	var regEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	var regPhone = /^[0-9]+$/;
+
+	$('form.validate-form').each(function() {
+		var form = $(this);
+		form.attr('novalidate', 'novalidate');
+		var successFlag = true;
+		var inputs = form.find('input, textarea, select');
+		// form validation function
+		function validateForm() {
+			successFlag = true;
+			inputs.each(checkField);
+			if (!successFlag) {
+				return false;
+			}
+		}
+		// check field
+		function checkField(i, obj) {
+			var currentObject = $(obj);
+			var currentParent = currentObject.parents('.row');
+			// not empty fields
+			if (currentObject.hasClass('required')) {
+				setState(currentParent, currentObject, !currentObject.val().length || currentObject.val() === currentObject.prop('defaultValue'));
+			}
+			// correct email fields
+			if (currentObject.hasClass('required-email')) {
+				setState(currentParent, currentObject, !regEmail.test(currentObject.val()));
+			}
+			// correct number fields
+			if (currentObject.hasClass('required-number')) {
+				setState(currentParent, currentObject, !regPhone.test(currentObject.val()));
+			}
+			// something selected
+			if (currentObject.hasClass('required-select')) {
+				setState(currentParent, currentObject, currentObject.get(0).selectedIndex === 0);
+			}
+		}
+		// set state
+		function setState(hold, field, error) {
+			hold.removeClass(errorClass).removeClass(successClass);
+			if (error) {
+				hold.addClass(errorClass);
+				field.one('focus', function() {hold.removeClass(errorClass).removeClass(successClass);});
+				successFlag = false;
+			} else {
+				hold.addClass(successClass);
+			}
+		}
+		// form event handlers
+		form.submit(validateForm);
+	});
+}
+
+/*
+ * jQuery Tooltip plugin
+ */
+;(function($){
+	$.fn.hoverTooltip = function(o) {
+		var options = $.extend({
+			tooltipStructure: '<div class="hover-tooltip"><div class="tooltip-text"></div></div>',
+			tooltipSelector: '.tooltip-text',
+			positionTypeX: 'right',
+			positionTypeY: 'top',
+			attribute:'title',
+			extraOffsetX: 10,
+			extraOffsetY: 10,
+			showOnTouchDevice: true
+		},o);
+		
+		// create tooltip
+		var tooltip = $('<div>').html(options.tooltipStructure).children().css({position:'absolute'});
+		var tooltipTextBox = tooltip.find(options.tooltipSelector);
+		var tooltipWidth, tooltipHeight;
+		
+		
+		// tooltip logic
+		function initTooltip(item) {
+			var tooltipText = item.attr(options.attribute);
+			item.removeAttr(options.attribute);
+			if(!tooltipText) return;
+
+			if(isTouchDevice) {
+				if(options.showOnTouchDevice) {
+					item.bind('touchstart', function(e) {
+						showTooltip(item, tooltipText, getEvent(e));
+						jQuery(document).one('touchend', hideTooltip);
+					});
+				}
+			} else {
+				item.bind('mouseenter', function(e) {
+					showTooltip(item, tooltipText, e);
+				}).bind('mouseleave', hideTooltip).bind('mousemove', moveTooltip);
+			}
+		}
+		function showTooltip(item, text, e) {
+			tooltipTextBox.html(text);
+			tooltip.appendTo(document.body).show();
+			tooltipWidth = tooltip.outerWidth(true);
+			tooltipHeight = tooltip.outerHeight(true);
+			moveTooltip(e, item);
+		}
+		function hideTooltip() {
+			tooltip.remove();
+		}
+		function moveTooltip(e) {
+			var top, left, x = e.pageX, y = e.pageY;
+
+			switch(options.positionTypeY) {
+				case 'top':
+					top = y - tooltipHeight - options.extraOffsetY;
+					break;
+				case 'center':
+					top = y - tooltipHeight / 2;
+					break;
+				case 'bottom':
+					top = y + options.extraOffsetY;
+					break;
+			}
+
+			switch(options.positionTypeX) {
+				case 'left':
+					left = x - tooltipWidth - options.extraOffsetX;
+					break;
+				case 'center':
+					left = x - tooltipWidth / 2;
+					break;
+				case 'right':
+					left = x + options.extraOffsetX;
+					break;
+			}
+			
+			tooltip.css({
+				top: top,
+				left: left
+			});
+		}
+		
+		// add handlers
+		return this.each(function(){
+			initTooltip($(this));
+		});
+	};
+	
+	// parse event
+	function getEvent(e) {
+		return e.originalEvent.changedTouches ? e.originalEvent.changedTouches[0] : e;
+	}
+	
+	// detect device type
+	var isTouchDevice = (function() {
+		try {
+			return ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch;
+		} catch (e) {
+			return false;
+		}
+	}());
+	
+}(jQuery));
+
+/*
+ * Mobile hover plugin
+ */
+;(function($){
+
+	// detect device type
+	var isTouchDevice = ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch,
+		isWinPhoneDevice = /Windows Phone/.test(navigator.userAgent);
+
+	// define events
+	var eventOn = (isTouchDevice && 'touchstart') || (isWinPhoneDevice && navigator.pointerEnabled && 'pointerdown') || (isWinPhoneDevice && navigator.msPointerEnabled && 'MSPointerDown') || 'mouseenter',
+		eventOff = (isTouchDevice && 'touchend') || (isWinPhoneDevice && navigator.pointerEnabled && 'pointerup') || (isWinPhoneDevice && navigator.msPointerEnabled && 'MSPointerUp') || 'mouseleave';
+
+	// event handlers
+	var toggleOn, toggleOff, preventHandler;
+	if(isTouchDevice || isWinPhoneDevice) {
+		// prevent click handler
+		preventHandler = function(e) {
+			e.preventDefault();
+		};
+
+		// touch device handlers
+		toggleOn = function(e) {
+			var options = e.data, element = $(this);
+
+			var toggleOff = function(e) {
+				var target = $(e.target);
+				if (!target.is(element) && !target.closest(element).length) {
+					element.removeClass(options.hoverClass);
+					element.off('click', preventHandler);
+					if(options.onLeave) options.onLeave(element);
+					$(document).off(eventOn, toggleOff);
+				}
+			};
+
+			if(!element.hasClass(options.hoverClass)) {
+				element.addClass(options.hoverClass);
+				element.one('click', preventHandler);
+				$(document).on(eventOn, toggleOff);
+				if(options.onHover) options.onHover(element);
+			}
+		};
+	} else {
+		// desktop browser handlers
+		toggleOn = function(e) {
+			var options = e.data, element = $(this);
+			element.addClass(options.hoverClass);
+			$(options.context).on(eventOff, options.selector, options, toggleOff);
+			if(options.onHover) options.onHover(element);
+		};
+		toggleOff = function(e) {
+			var options = e.data, element = $(this);
+			element.removeClass(options.hoverClass);
+			$(options.context).off(eventOff, options.selector, toggleOff);
+			if(options.onLeave) options.onLeave(element);
+		};
+	}
+
+	// jQuery plugin
+	$.fn.touchHover = function(opt) {
+		var options = $.extend({
+			context: this.context,
+			selector: this.selector,
+			hoverClass: 'hover'
+		}, opt);
+
+		$(this.context).on(eventOn, this.selector, options, toggleOn);
+		return this;
+	};
+}(jQuery));
+
+/*! http://mths.be/placeholder v2.0.7 by @mathias */
+;(function(window, document, $) {
+
+	// Opera Mini v7 doesn’t support placeholder although its DOM seems to indicate so
+	var isOperaMini = Object.prototype.toString.call(window.operamini) == '[object OperaMini]';
+	var isInputSupported = 'placeholder' in document.createElement('input') && !isOperaMini;
+	var isTextareaSupported = 'placeholder' in document.createElement('textarea') && !isOperaMini;
+	var prototype = $.fn;
+	var valHooks = $.valHooks;
+	var propHooks = $.propHooks;
+	var hooks;
+	var placeholder;
+
+	if (isInputSupported && isTextareaSupported) {
+
+		placeholder = prototype.placeholder = function() {
+			return this;
+		};
+
+		placeholder.input = placeholder.textarea = true;
+
+	} else {
+
+		placeholder = prototype.placeholder = function() {
+			var $this = this;
+			$this
+				.filter((isInputSupported ? 'textarea' : ':input') + '[placeholder]')
+				.not('.placeholder')
+				.bind({
+					'focus.placeholder': clearPlaceholder,
+					'blur.placeholder': setPlaceholder
+				})
+				.data('placeholder-enabled', true)
+				.trigger('blur.placeholder');
+			return $this;
+		};
+
+		placeholder.input = isInputSupported;
+		placeholder.textarea = isTextareaSupported;
+
+		hooks = {
+			'get': function(element) {
+				var $element = $(element);
+
+				var $passwordInput = $element.data('placeholder-password');
+				if ($passwordInput) {
+					return $passwordInput[0].value;
+				}
+
+				return $element.data('placeholder-enabled') && $element.hasClass('placeholder') ? '' : element.value;
+			},
+			'set': function(element, value) {
+				var $element = $(element);
+
+				var $passwordInput = $element.data('placeholder-password');
+				if ($passwordInput) {
+					return $passwordInput[0].value = value;
+				}
+
+				if (!$element.data('placeholder-enabled')) {
+					return element.value = value;
+				}
+				if (value == '') {
+					element.value = value;
+					// Issue #56: Setting the placeholder causes problems if the element continues to have focus.
+					if (element != safeActiveElement()) {
+						// We can't use `triggerHandler` here because of dummy text/password inputs :(
+						setPlaceholder.call(element);
+					}
+				} else if ($element.hasClass('placeholder')) {
+					clearPlaceholder.call(element, true, value) || (element.value = value);
+				} else {
+					element.value = value;
+				}
+				// `set` can not return `undefined`; see http://jsapi.info/jquery/1.7.1/val#L2363
+				return $element;
+			}
+		};
+
+		if (!isInputSupported) {
+			valHooks.input = hooks;
+			propHooks.value = hooks;
+		}
+		if (!isTextareaSupported) {
+			valHooks.textarea = hooks;
+			propHooks.value = hooks;
+		}
+
+		$(function() {
+			// Look for forms
+			$(document).delegate('form', 'submit.placeholder', function() {
+				// Clear the placeholder values so they don't get submitted
+				var $inputs = $('.placeholder', this).each(clearPlaceholder);
+				setTimeout(function() {
+					$inputs.each(setPlaceholder);
+				}, 10);
+			});
+		});
+
+		// Clear placeholder values upon page reload
+		$(window).bind('beforeunload.placeholder', function() {
+			$('.placeholder').each(function() {
+				this.value = '';
+			});
+		});
+
+	}
+
+	function args(elem) {
+		// Return an object of element attributes
+		var newAttrs = {};
+		var rinlinejQuery = /^jQuery\d+$/;
+		$.each(elem.attributes, function(i, attr) {
+			if (attr.specified && !rinlinejQuery.test(attr.name)) {
+				newAttrs[attr.name] = attr.value;
+			}
+		});
+		return newAttrs;
+	}
+
+	function clearPlaceholder(event, value) {
+		var input = this;
+		var $input = $(input);
+		if (input.value == $input.attr('placeholder') && $input.hasClass('placeholder')) {
+			if ($input.data('placeholder-password')) {
+				$input = $input.hide().next().show().attr('id', $input.removeAttr('id').data('placeholder-id'));
+				// If `clearPlaceholder` was called from `$.valHooks.input.set`
+				if (event === true) {
+					return $input[0].value = value;
+				}
+				$input.focus();
+			} else {
+				input.value = '';
+				$input.removeClass('placeholder');
+				input == safeActiveElement() && input.select();
+			}
+		}
+	}
+
+	function setPlaceholder() {
+		var $replacement;
+		var input = this;
+		var $input = $(input);
+		var id = this.id;
+		if (input.value == '') {
+			if (input.type == 'password') {
+				if (!$input.data('placeholder-textinput')) {
+					try {
+						$replacement = $input.clone().attr({ 'type': 'text' });
+					} catch(e) {
+						$replacement = $('<input>').attr($.extend(args(this), { 'type': 'text' }));
+					}
+					$replacement
+						.removeAttr('name')
+						.data({
+							'placeholder-password': $input,
+							'placeholder-id': id
+						})
+						.bind('focus.placeholder', clearPlaceholder);
+					$input
+						.data({
+							'placeholder-textinput': $replacement,
+							'placeholder-id': id
+						})
+						.before($replacement);
+				}
+				$input = $input.removeAttr('id').hide().prev().attr('id', id).show();
+				// Note: `$input[0] != input` now!
+			}
+			$input.addClass('placeholder');
+			$input[0].value = $input.attr('placeholder');
+		} else {
+			$input.removeClass('placeholder');
+		}
+	}
+
+	function safeActiveElement() {
+		// Avoid IE9 `document.activeElement` of death
+		// https://github.com/mathiasbynens/jquery-placeholder/pull/99
+		try {
+			return document.activeElement;
+		} catch (err) {}
+	}
+
+}(this, document, jQuery));
